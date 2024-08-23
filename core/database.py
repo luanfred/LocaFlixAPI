d8c27be07b645e4c3be3fb2041f673d030bc9f5a
luanfred <luanmatheussf4@gmail.com>
2024-08-23 14:14:57 -0300

    test: rename test route

--- core/database.py
+++ core/database.py
@@ -2,7 +2,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DB_URL = "mysql+pymysql://root:123mudar@mysql-db:3306/locadora"
+DB_URL = "mysql+pymysql://root:123mudar@localhost:3306/locadora"
 
 engine = create_engine(DB_URL, echo=True)
 
